19c94ff10d3d99c8acea5bbfc1728a77dd5f2a64
vkrav4enko <v.krav4enko@gmail.com>
2013-08-21 15:08:34 +0300

    first commit

---
+++ Drawing/LineDrawView.h
@@ -0,0 +1,13 @@
+//
+//  lineDrawView.h
+//  Drawing
+//
+//  Created by Владимир on 21.08.13.
+//  Copyright (c) 2013 Владимир. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface lineDrawView : UIView
+
+@end
---
+++ Drawing/LineDrawView.m
@@ -0,0 +1,31 @@
+//
+//  lineDrawView.m
+//  Drawing
+//
+//  Created by Владимир on 21.08.13.
+//  Copyright (c) 2013 Владимир. All rights reserved.
+//
+
+#import "lineDrawView.h"
+
+@implementation lineDrawView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
